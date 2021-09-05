701799ec7b8aa55ceedbb33f0cd5debcd3e6a020
Denis <oparenyuk02@gmail.com>
2021-09-05 17:12:02 +0300

    test

---
+++ js/main.js
@@ -0,0 +1,20 @@
+const brandBtn = document.querySelector('.brand__btn'),
+      brandList = document.querySelector('.brand__list--hide');
+
+
+brandBtn.addEventListener('click', () => {
+    brandList.classList.toggle('brand__list--hide');
+});
+
+
+
+
+ const headerToggle = document.querySelector('.header-nav__toggle'),
+       headerList = document.querySelector('.header-nav__list'),
+       hero = document.querySelector('.hero');
+
+
+headerToggle.addEventListener('click', () => {
+    headerList.classList.toggle('header-nav__list--active');
+    hero.classList.toggle('hero--active');
+ });
